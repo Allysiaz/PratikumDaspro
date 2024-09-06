88febf0bbbb4a1a8a1c5193b73400aa068d4109b
Allysiaz <juliettanadine@gmail.com>
2024-09-06 10:48:17 +0800

    Praktikum01

---
+++ Praktikum 01/Latihan13.java
@@ -0,0 +1,11 @@
+/**
+ * Latihan13
+ */
+public class Latihan13 {
+    public static void main(String[] args) {
+        System.out.print("**IDENTITAS DIRI**");
+        System.out.print("Nama Saya Nadine Jullietta Allysia");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    
+    }
+}
---
+++ Praktikum 01/Tugas13.java
@@ -0,0 +1,15 @@
+/**
+ * Tugas13
+ */
+public class Tugas13 {
+
+    public static void main(String[] args) {
+        System.out.println("**********IDENTITAS DIRI**********");
+        System.out.println("Nama     : Nadine Jullietta Allysia");
+        System.out.println("Absen    : 13");
+        System.out.println("Kelas    : SIB-1D");
+        System.out.println("Prodi    : D-IV Sistem Informasi Bisnis");
+        System.out.println("Jurusan  : Teknologi Informasi");
+
+    }
+}
