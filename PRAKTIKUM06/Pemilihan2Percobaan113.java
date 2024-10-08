a1e4d08cfad0c83fe3642af3f6f165e425455b3d
Allysiaz <juliettanadine@gmail.com>
2024-10-08 14:06:33 +0800

    Pemilihan2Percobaan113

--- PRAKTIKUM06/Pemilihan2Percobaan113.java
+++ PRAKTIKUM06/Pemilihan2Percobaan113.java
@@ -8,11 +8,11 @@ public class Pemilihan2Percobaan113 {
         System.out.println("Masukkan tahun: ");
         int tahun = sc.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0)
-                System.out.println("Tahun Kabisat");
-
-        } else
+        if ((tahun % 4 == 0 && tahun % 100 !=0) || tahun % 400 == 0) {
+            System.out.println("Tahun Kabisat");
+        }
+        else {
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
