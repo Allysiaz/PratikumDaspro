8e8473bc61c6e325101d81c8b7a87a0cdcb146b4
Allysiaz <juliettanadine@gmail.com>
2024-10-21 14:55:35 +0800

    SiakadFor13

--- PRAKTIKUM07/SiakadFor13.java
+++ PRAKTIKUM07/SiakadFor13.java
@@ -5,6 +5,7 @@ public class SiakadFor13 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0,terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -12,16 +13,20 @@ public class SiakadFor13 {
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
-            if (nilai > tertinggi) {
-                tertinggi = nilai;
-            }
             if (nilai < terendah) {
                 terendah = nilai;
             }
-
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + tidakLulus);
+
 
     }
 }
