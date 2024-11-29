eaca6980f95e65e41c646bf95f495e637c0681d6
Allysiaz <juliettanadine@gmail.com>
2024-11-29 20:53:31 +0800

    Kafe13

--- PRAKTIKUM11/Kafe13.java
+++ PRAKTIKUM11/Kafe13.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Kafe13 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15.000");
         System.out.println("2. Cappuccino - Rp 20.000");
@@ -10,9 +17,26 @@ public class Kafe13 {
         System.out.println("==========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int [] hargaItems = {15000, 20000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+        return hargaTotal;
+    }
     public static void main(String[] args) {
-            Menu();
-        }
+        Menu("Andi", true);
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("\nMasukkan nomer menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+    }
     
 }
     
